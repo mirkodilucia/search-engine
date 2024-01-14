279734d1b48afdd6af67578f0029cf31d808662f
Mirko Di Lucia <dlmrk95@gmail.com>
2024-01-14 17:44:04 +0100

    Given A Regex Pattern Should Return Text Replaced By Spaces

--- preprocessor/src/main/java/org/example/PreProcessor.java
+++ preprocessor/src/main/java/org/example/PreProcessor.java
@@ -5,6 +5,31 @@ import java.util.regex.Matcher;
 
 public class PreProcessor {
 
+    private static final String URL_MATCHER = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
+    private static final String HTML_TAGS_MATCHER = "<[^>]+>";
+    private static final String NON_DIGIT_MATCHER = "[^a-zA-Z ]";
+    private static final String MULTIPLE_SPACE_MATCHER = " +";
+    private static final String CONSECUTIVE_LETTERS_MATCHER = "(.)\\1{2,}";
+
+    private static final String CAMEL_CASE_MATCHER = "(?<=[a-z])(?=[A-Z])";
+
+    private static final String REPLACEMENT = "\s";
+
+    public String cleanUrl(StringBuilder result) {
+        return "";
+    }
+
+    public String cleanByRegex(String input, String regexPattern) {
+        StringBuilder result = new StringBuilder();
+
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            matcher.appendReplacement(result, REPLACEMENT);
+        }
+
+        return result.toString();
+    }
 
     public String cleanText(String input) {
         StringBuilder result = new StringBuilder();
--- preprocessor/src/test/java/PreProcessorTest.java
+++ preprocessor/src/test/java/PreProcessorTest.java
@@ -1,8 +1,21 @@
+import jdk.jfr.Description;
 import org.example.PreProcessor;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PreProcessorTest {
+
+    // Given A Regex Pattern Should Return Text Replaced By Spaces
+    @Test
+    public void cleanByRegex_givenARegexPatternShouldReturnTextReplacedBySpaces() {
+        String input = "Replace this pattern: ABC123 with XYZ789.";
+        String regexPattern = "[A-Z]{3}\\d{3}";
+        String actualResult = new PreProcessor().cleanByRegex(input, regexPattern);
+
+        String expectedResult = "Replace this pattern:   with  ";
+        assertEquals(expectedResult, actualResult);
+    }
+
 
     @Test
     public void testRegexReplacement_shouldChangePatternWithSpace() {
